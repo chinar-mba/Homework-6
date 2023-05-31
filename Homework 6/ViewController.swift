a9507c4b936dc16671a48b57bf827373b47ba23e
chinar-mba <chinar.mba@gmail.com>
2023-05-30 23:42:57 -0400

    Initial Commit

---
+++ Homework 6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 6
+//
+//  Created by Chinara on 5/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
